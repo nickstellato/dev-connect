26e5ea0a36cb9249e633e3d6036ea66aae8871a6
Nick Stellato <nick@nickstellato.com>
2018-07-08 16:28:23 -0500

    Login now responds with a token when login is matched

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://nick:j6nU16^hN2yAQZH2eYbb&Qv7*B&3Y@ds129801.mlab.com:29801/dev-connect-2018"
+    "mongodb://nick:j6nU16^hN2yAQZH2eYbb&Qv7*B&3Y@ds129801.mlab.com:29801/dev-connect-2018",
+  secretOrKey: "secret"
 };
