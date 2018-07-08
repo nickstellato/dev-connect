aa040e8f2e4ab6a5934d1e81da9683c9d30f34b9
Nick Stellato <nick@nickstellato.com>
2018-07-08 17:19:53 -0500

    added length of name validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load user model
 const User = require("../../models/user");
 
@@ -18,6 +21,13 @@ router.get("/test", (req, res) => res.json({ msg: "Users works" }));
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "email already exists" });
